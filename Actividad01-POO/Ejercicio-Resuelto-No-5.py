5fb1af7b7e28944bf362e7fb9d712ed0357cb47e
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-08-19 10:57:16 -0500

    Codigo del ejercicio 5

---
+++ Actividad01-POO/Ejercicio-Resuelto-No-5.py
@@ -0,0 +1,16 @@
+""" Ejercicio Resuelto No 5,
+ Realizado por Mariana Arias Montoya """
+
+# Se declaran las variables dadas a utilizar
+suma = 0
+x = 20
+y = 40
+
+# Se cambian las variables suma y x por sus nuevos valores
+suma = suma + x
+x = x + y**2
+
+# Sobre la variable suma se hace la operación matemátia indicada 
+suma = suma + (x/y)
+
+print(f'La suma es: {suma}')
