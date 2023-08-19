8de4e28a856709463b6de17bb08018e61c1499c2
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-08-19 10:46:29 -0500

    Mejorando ejercicio 4, y creando nuevos archivos

--- Actividad01-POO/Ejercicio-Resuelto-No-4.py
+++ Actividad01-POO/Ejercicio-Resuelto-No-4.py
@@ -1,10 +1,10 @@
-""" Ejercicio Resuelto No 4 """
-""" Realizado por Mariana Arias Montoya """
+""" Ejercicio Resuelto No 4,
+ Realizado por Mariana Arias Montoya """
 
-ageJuan = int(input('Ingrese la edad de Juan: '))
+ageJuan = int(input('Ingrese la edad de Juan: ')) 
 
-ageAlberto = 2*(ageJuan/3)
-ageAna = 4*(ageJuan/3)
-ageMama = ageAlberto + ageJuan + ageAna
+ageAlberto = 2*(ageJuan/3) #Calculando la edad de Alberto
+ageAna = 4*(ageJuan/3) #Calculando la edad de Ana
+ageMama = ageAlberto + ageJuan + ageAna #Calculando la edad de la Mama 
 
 print(f'La edades son: Alberto: {int(ageAlberto)}, Juan: {ageJuan}, Ana: {int(ageAna)}, Mama: {int(ageMama)}')
