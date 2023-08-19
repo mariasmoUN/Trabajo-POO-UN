091f551bd0ce3b6b8d16a8737829a054e2d2e2c8
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-08-19 10:30:39 -0500

    Solucion ejercicio 4

---
+++ Actividad01-POO/Ejercicio-Resuelto-No-4.py
@@ -0,0 +1,10 @@
+""" Ejercicio Resuelto No 4 """
+""" Realizado por Mariana Arias Montoya """
+
+ageJuan = int(input('Ingrese la edad de Juan: '))
+
+ageAlberto = 2*(ageJuan/3)
+ageAna = 4*(ageJuan/3)
+ageMama = ageAlberto + ageJuan + ageAna
+
+print(f'La edades son: Alberto: {int(ageAlberto)}, Juan: {int(ageJuan)}, Ana: {int(ageAna)}, Mama: {int(ageMama)}')
