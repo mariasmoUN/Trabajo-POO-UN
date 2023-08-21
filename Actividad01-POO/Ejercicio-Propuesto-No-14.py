11dd9c64f43e169ba2cbd6f28190f3ae326c4d9a
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-08-21 12:28:35 -0500

    Corrigiendo ejercicio 5

---
+++ Actividad01-POO/Ejercicio-Propuesto-No-14.py
@@ -0,0 +1,2 @@
+""" Ejercicio propuesto No 14,
+ Realizado por Mariana Arias Montoya """
--- Actividad01-POO/Ejercicio-Resuelto-No-5.py
+++ Actividad01-POO/Ejercicio-Resuelto-No-5.py
@@ -1,6 +1,8 @@
 """ Ejercicio Resuelto No 5,
  Realizado por Mariana Arias Montoya """
 
+import math # importamos la librería math para algunos cálculos
+
 # Se declaran las variables dadas a utilizar y se piden los valores para X y Y
 suma = 0
 x = int(input('Ingrese un valor para x: '))
@@ -8,7 +10,7 @@ y = int(input('Ingrese un valor para y: '))
 
 # Se cambian las variables suma y x por sus nuevos valores
 suma = suma + x
-x = x + y**2
+x = x + math.pow(y, 2)
 
 # Sobre la variable suma se hace la operación matemátia indicada 
 suma = suma + (x/y)
