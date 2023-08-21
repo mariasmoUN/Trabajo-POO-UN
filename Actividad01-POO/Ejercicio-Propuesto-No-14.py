e69e5249c93a845976213d12b5527e1ee2aad744
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-08-21 12:44:21 -0500

    Ejercicio 14

--- Actividad01-POO/Ejercicio-Propuesto-No-14.py
+++ Actividad01-POO/Ejercicio-Propuesto-No-14.py
@@ -1,2 +1,11 @@
 """ Ejercicio propuesto No 14,
  Realizado por Mariana Arias Montoya """
+
+import math #importamos la librería math para algunos cálculos
+
+num = int(input('Ingrese un numero: '))
+
+cuadrado = int(math.pow(num, 2)) #calculamos el valor de su cuadrado
+cubo = int(math.pow(num, 3)) #calculamos el valor de su cubo
+
+print(f'El cuadrado y el cubo del numero {num} es: {cuadrado} y {cubo}')
