d1fda5195772f7a8cf2c5f7f802c42798b5b8495
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-09-01 12:12:52 -0500

    Ejercicio 7

---
+++ Actividad02-POO/Ejercicio-resuelto-7.py
@@ -0,0 +1,14 @@
+""" Ejercicio resuelto No 7,
+ Realizado por Mariana Arias Montoya """
+
+#Declaramos las variables a usar y pedimos su valor por teclado
+a = int(input('Ingrese el valor de A: '))
+b = int(input('Ingrese el valor de B: '))
+
+#Iniciamos un ciclo condicional para comparar ambos valores 
+if a > b:
+    print('A es mayor a B')
+elif a == b:
+    print('A es igual a B')
+else:
+    print('A es menor a B')
