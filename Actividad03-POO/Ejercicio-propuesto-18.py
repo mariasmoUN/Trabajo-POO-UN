9efb9b74499b21818fb0b5ab37b6c2811899a4aa
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-09-09 13:10:55 -0500

    Corrección ejercicio 18

--- Actividad03-POO/Ejercicio-propuesto-18.py
+++ Actividad03-POO/Ejercicio-propuesto-18.py
@@ -2,55 +2,60 @@
  Realizado por Mariana Arias Montoya """
 
 # Importamos la librería tkinter para realizar la interfaz grafica
+
 import tkinter as tk
 from tkinter import ttk
 
-def calcularSalario():
-    codigoEmpl = int(caja_codigoEmpl.get())
-    name = caja_name.get()
-    horasTrab = int(caja_horasTrab.get())
-    valorHora = int(caja_valorHora.get())
-    porRetencion = float(caja_porRetencion.get())
+class salario(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-    salarioB = horasTrab * valorHora
-    retencion = int(salarioB * porRetencion)
-    salarioN = salarioB - retencion
+        self.etiqueta_codigoEmpl = ttk.Label(parent, text="Ingrese el código del empleado:")
+        self.etiqueta_codigoEmpl.place(x=20, y=20)
+        self.caja_codigoEmpl = ttk.Entry(parent)
+        self.caja_codigoEmpl.place(x=280, y=20, width=100)
 
-    etiqueta_resultado.config(text= f'{codigoEmpl} - {name} - ${salarioB} - ${salarioN}')
+        self.etiqueta_name = ttk.Label(parent, text="Ingrese el nombre del empleado:")
+        self.etiqueta_name.place(x=20, y=50)
+        self.caja_name = ttk.Entry(parent)
+        self.caja_name.place(x=280, y=50, width=100)
+
+        self.etiqueta_horasTrab = ttk.Label(parent, text="Ingrese el total de horas trabajadas al mes:")
+        self.etiqueta_horasTrab.place(x=20, y=80)
+        self.caja_horasTrab = ttk.Entry(parent)
+        self.caja_horasTrab.place(x=280, y=80, width=100)
+
+        self.etiqueta_valorHora = ttk.Label(parent, text="Ingrese el valor de la hora de trabajo:")
+        self.etiqueta_valorHora.place(x=20, y=110)
+        self.caja_valorHora = ttk.Entry(parent)
+        self.caja_valorHora.place(x=280, y=110, width=100)
+
+        self.etiqueta_porRetencion = ttk.Label(parent, text="Ingrese el porcentaje de retención en la fuente:")
+        self.etiqueta_porRetencion.place(x=20, y=140)
+        self.caja_porRetencion = ttk.Entry(parent)
+        self.caja_porRetencion.place(x=280, y=140, width=100)
+
+        self.boton_calcular = ttk.Button(parent, text="Calcular salario", command=self.calcularSalario)
+        self.boton_calcular.place(x=150, y=180, width=100)
+
+        self.etiqueta_resultado = ttk.Label(parent)
+        self.etiqueta_resultado.place(x=20, y=220)
+
+    def calcularSalario(self):
+        codigoEmpl = int(self.caja_codigoEmpl.get())
+        name = self.caja_name.get()
+        horasTrab = int(self.caja_horasTrab.get())
+        valorHora = int(self.caja_valorHora.get())
+        porRetencion = float(self.caja_porRetencion.get())
+
+        salarioB = horasTrab * valorHora
+        retencion = int(salarioB * porRetencion)
+        salarioN = salarioB - retencion
+
+        self.etiqueta_resultado.config(text= f'{codigoEmpl} - {name} - ${salarioB} - ${salarioN}')
 
 root = tk.Tk()
 root.title("Ejercicio propuesto No 18")
 root.config(width=400, height=260)
-
-etiqueta_codigoEmpl = ttk.Label(text="Ingrese el código del empleado:")
-etiqueta_codigoEmpl.place(x=20, y=20)
-caja_codigoEmpl = ttk.Entry()
-caja_codigoEmpl.place(x=280, y=20, width=100)
-
-etiqueta_name = ttk.Label(text="Ingrese el nombre del empleado:")
-etiqueta_name.place(x=20, y=50)
-caja_name = ttk.Entry()
-caja_name.place(x=280, y=50, width=100)
-
-etiqueta_horasTrab = ttk.Label(text="Ingrese el total de horas trabajadas al mes:")
-etiqueta_horasTrab.place(x=20, y=80)
-caja_horasTrab = ttk.Entry()
-caja_horasTrab.place(x=280, y=80, width=100)
-
-etiqueta_valorHora = ttk.Label(text="Ingrese el valor de la hora de trabajo:")
-etiqueta_valorHora.place(x=20, y=110)
-caja_valorHora = ttk.Entry()
-caja_valorHora.place(x=280, y=110, width=100)
-
-etiqueta_porRetencion = ttk.Label(text="Ingrese el porcentaje de retención en la fuente:")
-etiqueta_porRetencion.place(x=20, y=140)
-caja_porRetencion = ttk.Entry()
-caja_porRetencion.place(x=280, y=140, width=100)
-
-boton_calcular = ttk.Button(text="Calcular salario", command=calcularSalario)
-boton_calcular.place(x=150, y=180, width=100)
-
-etiqueta_resultado = ttk.Label()
-etiqueta_resultado.place(x=20, y=220)
-
+app = salario(root)
 root.mainloop()
